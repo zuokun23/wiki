f3f1d249a0fc78531a33efb72f7afa6913419ec0
zuokun23 <1291136919@qq.com>
2021-04-22 13:35:48 +0800

    修改启动文案

--- src/main/java/com/carlos/wiki/WikiApplication.java
+++ src/main/java/com/carlos/wiki/WikiApplication.java
@@ -1,13 +1,22 @@
 package com.carlos.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication();
+        ConfigurableEnvironment environment = app.run(WikiApplication.class, args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://localhost:{}",environment.getProperty("server.port"));
     }
 
 }
