cad1dcca09ce8ab28204e63a47360e5fa01a5afb
zuokun23 <1291136919@qq.com>
2021-04-25 14:19:34 +0800

    使用axios调用后端接口/ebook/list

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,8 +3,6 @@
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
       >
         <a-sub-menu key="sub1">
@@ -55,8 +53,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
+  setup(){
+    console.log("setup");
+    axios.get("http://localhost:8882/ebook/list?name=入门").then(
+        (response) => {
+          console.log(response);
+        }
+    )}
 });
 </script>
