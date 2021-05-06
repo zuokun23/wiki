9a909448c34e774f814fe604a315366e4cbb7300
zuokun23 <1291136919@qq.com>
2021-05-06 19:39:49 +0800

    增加拦截器，打印接口耗时

---
+++ src/main/java/com/carlos/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,24 @@
+package com.carlos.wiki.config;
+
+import com.carlos.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    //增加拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")//针对所有的请求都去打印接口的耗时
+                .excludePathPatterns("/login");
+    }
+}
