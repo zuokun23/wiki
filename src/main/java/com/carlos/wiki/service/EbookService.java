7c9c2e41515167444b28f4fd816f45caccae095c
zuokun23 <1291136919@qq.com>
2021-05-08 15:13:24 +0800

    使用PageHelper实现后端分页

--- src/main/java/com/carlos/wiki/service/EbookService.java
+++ src/main/java/com/carlos/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.carlos.wiki.mapper.EbookMapper;
 import com.carlos.wiki.req.EbookReq;
 import com.carlos.wiki.resp.EbookResp;
 import com.carlos.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -17,15 +21,24 @@ public class EbookService {
 
     @Resource
     private EbookMapper ebookMapper;
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     public List<EbookResp> list(EbookReq req){
+
 
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数: {}", pageInfo.getPages());
+
         //持久层返回List<Ebook>需要转换成List<EbookResp>再返回controller
         List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
 
